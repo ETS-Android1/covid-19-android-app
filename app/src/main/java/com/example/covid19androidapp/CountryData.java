36d2ea9696bfb71e391de6edeae7b629be8d0c92
andrei <git1@email.com>
2021-03-05 17:27:41 +0100

    added spinner CountryData

---
+++ app/src/main/java/com/example/covid19androidapp/CountryData.java
@@ -0,0 +1,15 @@
+package com.example.covid19androidapp;
+
+public class CountryData {
+
+    public  static final String[] countryNames = {
+
+            "IND", "USA", "MYS", "AFG", "MEX"
+    };
+
+    public  static final int[] countryFlag = {
+
+            R.drawable.india, R.drawable.uk, R.drawable.malesiya, R.drawable.afganistan, R.drawable.maxico
+    };
+
+}
--- app/src/main/java/com/example/covid19androidapp/MainActivity.java
+++ app/src/main/java/com/example/covid19androidapp/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.covid19androidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView flags;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+        flags = findViewById(R.id.flag);
+
+        spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,
+                CountryData.countryNames));
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                flags.setImageResource(CountryData.countryFlag[spinner.getSelectedItemPosition()]);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
     }
 }
