67fb8596b477ebf386fddaab8acd06ba867c96d3
andrei <git1@email.com>
2021-03-08 17:43:41 +0100

    hooked back button to main activity

--- app/src/main/java/com/example/covid19androidapp/Statistics.java
+++ app/src/main/java/com/example/covid19androidapp/Statistics.java
@@ -2,13 +2,27 @@ package com.example.covid19androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Statistics extends AppCompatActivity {
+
+    ImageView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+
+        back = findViewById(R.id.imageView);
+
+        back.setOnClickListener(view -> {
+
+            Intent i =new Intent(Statistics.this, MainActivity.class);
+            startActivity(i);
+            finish();
+
+        });
     }
 }
